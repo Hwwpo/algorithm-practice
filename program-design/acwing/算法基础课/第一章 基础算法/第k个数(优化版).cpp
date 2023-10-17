71f87f1033638e3da5474eab2157cee1c4dcaf5c
ppy <1422322819@qq.com>
2023-10-17 21:46:49 +0800

    整理了项目，删除了与源码无关的配置文件

--- program-design/acwing/算法基础课/第一章 基础算法/第k个数(优化版).cpp
+++ program-design/acwing/算法基础课/第一章 基础算法/第k个数(优化版).cpp
@@ -1,37 +1,42 @@
+// 题目链接：https://www.acwing.com/problem/content/788/
+
+// 思路：
+// 使用传统的快排的时间复杂度是nlogn，但是使用快速选择排序时间复杂度为线性
+// 使用快排思路将数组分成两部分后，左边的数是严格小于右边的数的，因此如果k小于左边数列的长度，答案一定在左边
+// 就不需要排序右边的数组了
+
 #include<iostream>
 
 using namespace std;
 
-const int N = 1e6 + 10;
+const int N = 100010;
 
 int n, k;
-int q[N];
+int p[N];
 
 int quick_sort(int l, int r, int k)
 {
-	if(l >= r) return q[l];
-	
-	int x = q[(l+r)/2], i = l - 1, j = r + 1;
+	if(l == r) return p[l];
+	int x = p[l], i = l - 1, j = r + 1;
 	while(i < j)
 	{
-		do i++; while(q[i] < x);
-		do j--; while(q[j] > x);
-		if(i < j) swap(q[i], q[j]);
-	 } 
-	 int sl = j - l + 1;
-	 if(k > sl) return quick_sort(j + 1, r, k - sl);
-	 return quick_sort(l, j, k);
-	 
+		do i++; while (p[i] < x);
+		do j--; while (p[j] > x);
+		if(i < j) swap(p[i], p[j]);
+	}
+	int sl = j - l + 1;
+	if (k <= sl) return quick_sort(l, j, k);
+	
+	return quick_sort(j + 1, r, k - sl);
 }
 
 int main()
 {
-	scanf("%d%d", &n, &k);
+	cin >> n >> k;
 	for(int i = 0; i < n; i++)
-	{
-		scanf("%d", &q[i]);
-	}
+		cin >> p[i];
 	
-	printf("%d", quick_sort(0, n-1, k));
+	cout << quick_sort(0, n - 1, k);
+	
 	return 0;
 }
